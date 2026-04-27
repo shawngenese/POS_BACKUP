620201d1db7343a8154711b77757791f06e01e2a
shawngenese <shawngenese25@gmail.com>
2026-04-28 01:40:20 +0800

    sales report form
    okie? okie

---
+++ SalesReport.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Point_Of_Sale
+{
+    public partial class SalesReport : Form
+    {
+        public SalesReport()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDashboard_Click(object sender, EventArgs e)
+        {
+            Dashboard dashboard = new Dashboard();
+            dashboard.Show();
+            this.Close();
+        }
+
+        private void btnProducts_Click(object sender, EventArgs e)
+        {
+            ProductManagement productManagement = new ProductManagement();
+            productManagement.Show();
+            this.Close();
+        }
+
+        private void btnCategories_Click(object sender, EventArgs e)
+        {
+            CategoryManagement categoryManagement = new CategoryManagement();
+            categoryManagement.Show();
+            this.Close();
+        }
+
+        private void btnUsers_Click(object sender, EventArgs e)
+        {
+            UserManagement userManagement = new UserManagement();
+            userManagement.Show();
+            this.Close();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to logout?", "Logout Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Login login = new Login();
+                login.Show();
+                this.Close();
+            }
+        }
+    }
+}
